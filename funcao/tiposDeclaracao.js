fb388b97b77e25dfb54d0d9347be7ebc005a9149
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-17 18:37:44 -0300

    Função

---
+++ funcao/tiposDeclaracao.js
@@ -0,0 +1,20 @@
+console.log(soma(3, 4))
+
+//function declaration
+function soma(x, y){
+    return x + y
+}
+
+//function expression
+const sub = function(x, y){
+    return x - y
+}
+
+console.log(sub(10, 5))
+
+//named function expression
+const mult = function mult(x, y){
+    return x * y
+}
+
+console.log(mult(5, 5))
