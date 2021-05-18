228d136f4883ebdf536324edc5ca3f9f07a14090
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-18 17:16:03 -0300

    Exercicios JavaScript

---
+++ Exercicios JavaScript/Exercicio20.js
@@ -0,0 +1,66 @@
+function sacarDinheiro(valorSaque){
+    let nota1 = 0
+    let nota5 = 0
+    let nota10 = 0
+    let nota50 = 0
+    let nota100 = 0
+    let valorNota = calcValorNotas(valorSaque)
+    while(valorSaque >= valorNota){
+        switch(valorNota){
+            case 100:
+                valorSaque -= 100
+                nota100++
+                break
+            case 50:
+                valorSaque -= 50
+                nota50++
+                break
+            case 10:
+                valorSaque -= 10
+                nota10++
+                break
+            case 5:
+                valorSaque -= 5
+                nota5++
+                break
+            case 1:
+                valorSaque -= 1
+                nota1++
+                break               
+        }
+
+        valorNota = calcValorNotas(valorSaque)
+    }
+    return elaborarResultado(nota100, nota50, nota10, nota5, nota1)
+}
+function calcValorNotas(valorSaque){
+    if(valorSaque >= 100){
+        return 100
+    }else if(valorSaque >= 50){
+        return 50
+    }else if(valorSaque >= 10){
+        return 10
+    }else if(valorSaque >= 5){
+        return 5
+    }else if(valorSaque >= 1){
+        return 1
+    }
+}
+
+function elaborarResultado(nota100, nota50, nota10, nota5, nota1){
+    let resultado = ''
+    if(nota100 > 0){
+        resultado += `${nota100} de R$:100,00`
+    }else if(nota50 > 0){
+        resultado += `${nota50} de R$:50,00`
+    }else if(nota10 > 0){
+        resultado += `${nota10} de R$10,00`
+    }else if(nota5 > 0){
+        resultado += `${nota5} de R$5,00`
+    }else if(nota1 > 0){
+        resultado += `${nota1} de R$1,00` 
+    }
+    return resultado
+}
+
+console.log(sacarDinheiro(140))
