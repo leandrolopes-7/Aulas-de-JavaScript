64bfb2fac38a5c8cc3572921e10aa713ba9dc3d9
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-19 17:55:57 -0300

    Exercicios JavaScript

---
+++ Exercicios JavaScript/Exercicio34.js
@@ -0,0 +1,11 @@
+function comparaString(string1, string2){
+    if(string1 == string2){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(comparaString('ola', 'alo'))
+console.log(comparaString('ola', 'Ola'))
+console.log(comparaString('Ola', 'tudo'))
