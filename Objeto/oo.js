e87371bba5cbf20898843912b1c00ad836da7ccf
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-20 20:39:28 -0300

    Aula Objetos

---
+++ Objeto/oo.js
@@ -0,0 +1,22 @@
+ //CODIGO NAO EXECUTAVEL
+
+ //PROCEDURAL
+ processamento(valor1, valor2, valor3)
+
+ //OO
+ objeto = {
+     valor1,
+     valor2,
+     valor3,
+     processamento(){
+         //...
+     }
+ }
+
+ objeto.processamento()
+
+ //Principios importantes:
+ // 1. abstração
+ // 2. encapsulamento
+ // 3. Herança (prototype)
+ // 4. polimorfismo
