1e810c01890bc344cbeb28a25d18976339425a66
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-28 20:06:27 -0300

    exercicio JavaScript

---
+++ Exercicios JavaScript Part 2/exercicio05.js
@@ -0,0 +1,11 @@
+function maiorOuIgual(Numero1, numero2){
+    if(Numero1 <= numero2 && Numero1 === numero2){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+
+maiorOuIgual(1, "1")
+maiorOuIgual(1, 1)
+maiorOuIgual(5, 1)
