d8ebc0b80d626b4ca62d9d62584da488a801b8d3
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-28 19:05:25 -0300

    JavaScript Concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhos = ['Leandro', 'Mariana']
+const pais = ['Antonio', 'Quiteria']
+const todos = pais.concat(filhos)
+console.log(todos, pais, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
