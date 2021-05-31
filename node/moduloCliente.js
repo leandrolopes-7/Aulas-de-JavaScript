d607f655b85210be6f97349d533bf3977ff8d9d8
leandrolopes-7 <leandrolopes_7@live.com>
2021-05-31 11:19:50 -0300

    nodeJS

---
+++ node/moduloCliente.js
@@ -0,0 +1,9 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.saudacao)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+
+console.log(moduloB.bomdia)
+console.log(moduloB.boaNoite())
