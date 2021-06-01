9b912a27225c33eba5c6534164606d50d937ace2
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-01 19:59:48 -0300

    Promise

---
+++ esnext/promise_2.js
@@ -0,0 +1,26 @@
+setTimeout(function(){
+    console.log('Executando...')
+
+    setTimeout(function(){
+        console.log('executando...')
+
+        setTimeout(function() {
+            console.log('Executando...')
+        }, 2000)
+
+    },2000)
+
+}, 2000)
+
+function esperarPor(tempo = 2000){
+    return new Promise(function (resolve){
+        setTimeout(function() {
+            console.log('Executando Promise...')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(esperarPor)
