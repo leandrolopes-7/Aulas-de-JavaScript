c0d4f7b35c1fe1ed32dd9d492d516cc94ddb058e
leandrolopes-7 <leandrolopes_7@live.com>
2021-06-07 15:06:42 -0300

    formulario html

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabens</h1>')
+    resp.send('<h1>Parabens. Usuário incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabens. Usuário Alterado</h1>')
 })
 
 app.listen(3003)
